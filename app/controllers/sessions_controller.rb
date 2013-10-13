f598e474b573106b777093ce5c34c61d8b52aa0e
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-13 14:46:44 -0700

    Define create action for Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by_credentials(params["user"])
+    user.reset_session_token! if user
+    session[:session_token] = user.session_token
     
+    redirect_to cats_url
   end
   
   def new
@@ -8,6 +12,4 @@ class SessionsController < ApplicationController
     
     render :new
   end
-  
-  
 end
