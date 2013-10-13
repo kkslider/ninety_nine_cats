4ffe6da9dccfd7f95e28158325a935e1682e2717
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-13 13:55:08 -0700

    Create users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131013045133) do
+ActiveRecord::Schema.define(:version => 20131013205001) do
 
   create_table "cat_rental_requests", :force => true do |t|
     t.integer  "cat_id",                            :null => false
@@ -33,5 +33,16 @@ ActiveRecord::Schema.define(:version => 20131013045133) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "user_name",       :null => false
+    t.string   "password_digest", :null => false
+    t.string   "session_token",   :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "users", ["session_token"], :name => "index_users_on_session_token", :unique => true
+  add_index "users", ["user_name"], :name => "index_users_on_user_name", :unique => true
 
 end
